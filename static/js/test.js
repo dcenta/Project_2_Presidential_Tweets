765f5fce672060fcbf42c2d66a48ae453990240b
Corbin Leho <corbinleho@yahoo.com>
2019-03-11 18:17:39 -0400

    fixed

--- static/js/test.js
+++ static/js/test.js
@@ -25,7 +25,7 @@ function buildCandle() {
     //Candlestick Trace
     var trace = {
         type: "candlestick",
-        name: name,
+        name: name,//
         x: dates,
         high: highPrices,
         low: lowPrices,
@@ -77,17 +77,23 @@ function buildApproval() {
         console.log(response)
         var datesTw = response.map(data => data.Date);
         var Retweet = response.map(data => data.Retweet_Count);
-        var Tweets = response.map(data => data.Tweets.substring(0, 100));
+        var Tweets = response.map(data => data.Tweets.substring(0, 95));
+        var Tweets2 = response.map(data => data.Tweets.substring(96, 190));
+        var Tweets3 = response.map(data => data.Tweets.substring(191, 280));
 
+        //hovertext: ['A<br>a', 'B<br>b', 'C<br>c'],
+        //hoverinfo: 'text'
+        
     var trace1 = {
         type: "scatter",
         mode: "lines",
-        name: name,
+        //customdata: Tweets,
+        name: 'Approval',
         x: dates,
         y: approval,
-        text: Tweets,
-        textposition: 'bottom',
-        hoverinfo: 'trace1',
+        hovertext: [`${Tweets}<br>${Tweets2}<br>${Tweets3}`],
+        //textposition: 'bottom',
+        hoverinfo: 'text',
         line: {
             color: "#dc3545",
         }
@@ -95,11 +101,11 @@ function buildApproval() {
     var trace2 = {
         type: "scatter",
         mode: "lines",
-        name: name,
+        name: 'Dissapproval',
         x: dates,
         y: disapproval,
-        //text: Tweets,
-        //textposition: 'bottom',
+        text: Tweets,
+        textposition: 'bottom',
         hoverinfo: 'trace2',
         line: {
             color: "#007bff"
@@ -126,18 +132,35 @@ function buildApproval() {
     })
 }
 
+
+// Event handlers are just normal functions that can do anything you want
+
+// function wordCloud() {
+//     var chartDiv = d3.select("#bigChart");
+//     chartDiv.html("");
+//     chartDiv.innerHTML="<img src='wordcloud.svg' />";
+// }
+// function wordCloud() {
+//     var chartDiv = d3.select("#bigChart");
+//     chartDiv.html("");
+//     document.getElementById("bigChart").html="<img src='wordcloud.svg' />";
+// }
+//document.getElementById("surprise").innerHTML="<img src='images/aftersurprise.png' />";
+
 // Getting a reference to the buttons on the page with the id property
 var button = d3.select("#cs");
 var button2 = d3.select("#ap");
+var button3 = d3.select("#wc");
 
 // We can use the `on` function in d3 to attach an event to the handler function
 button.on("click", buildCandle);
 button2.on("click", buildApproval);
-//button3.on("click", chartDiv = img src = '//assets/images/wordmap.png');
+//button3.on("click", wordCloud);
 
-// buildPlot()
-// buildPlot2()
-
+button3.on("click", function() {
+    //document.getElementById('secondchart').style.display = "true";
+    d3.select("#bigchart").html("<img src='https://www.prgarnett.net/wp-content/uploads/2017/12/TrumpWords.png'' alt='giphy'>");
+  });
 // var chartDiv = d3.select("#bigchart");
 // chartDiv.html("")
 
